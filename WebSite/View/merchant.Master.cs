5fd2512678ca97f890b54013bb5f1e483e805ed4
longer310 <814300610@qq.com>
2014-07-24 20:36:20 +0800

    商户母版页 忘记提交了 更改项目文件

---
+++ WebSite/View/merchant.Master.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Backstage.Core;
+using Backstage.Core.Entity;
+
+namespace WebSite.View
+{
+    /// <summary>
+    /// 商户模板页
+    /// </summary>
+    public class MerchantMasterBasePage : System.Web.UI.MasterPage
+    {
+        public Account Merchant {
+            get { return AccountHelper.GetCurUser(); }}
+    }
+    public partial class MerchantMaster : MerchantMasterBasePage
+    {
+        public string UserName { get; set; }
+        public float Money { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Merchant != null)
+            {
+                UserName = Merchant.UserName;
+                Money = Merchant.Money;
+            }
+        }
+    }
+}
